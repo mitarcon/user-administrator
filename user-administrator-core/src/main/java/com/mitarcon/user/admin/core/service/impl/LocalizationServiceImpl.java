52f4824775d0f8676961ebc564da958cc1a36e6d
mitarcon <mitarcon@gmail.com>
2019-06-03 20:05:36 -0500

    añadir cache al proyecto

--- user-administrator-core/src/main/java/com/mitarcon/user/admin/core/service/impl/LocalizationServiceImpl.java
+++ user-administrator-core/src/main/java/com/mitarcon/user/admin/core/service/impl/LocalizationServiceImpl.java
@@ -3,6 +3,8 @@ package com.mitarcon.user.admin.core.service.impl;
 import java.util.List;
 
 import org.cactoos.list.ListOf;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import com.mitarcon.user.admin.core.model.Localization;
@@ -20,6 +22,7 @@ public class LocalizationServiceImpl implements LocalizationService {
   }
 
   @Override
+  @Cacheable("localizaton")
   public List<Localization> findAll() {
     return new ListOf<Localization>(localizationRepository.findAll());
   }
@@ -30,11 +33,13 @@ public class LocalizationServiceImpl implements LocalizationService {
   }
 
   @Override
+  @CacheEvict(cacheNames = "localizaton", allEntries = true)
   public Localization create(Localization localization) {
     return localizationRepository.save(localization);
   }
 
   @Override
+  @CacheEvict(cacheNames = "localizaton", allEntries = true)
   public Localization update(Long id, Localization localization) {
     // TODO Auto-generated method stub
     return null;
--- user-administrator-core/src/main/java/com/mitarcon/user/admin/core/service/impl/StoreServiceImpl.java
+++ user-administrator-core/src/main/java/com/mitarcon/user/admin/core/service/impl/StoreServiceImpl.java
@@ -3,6 +3,8 @@ package com.mitarcon.user.admin.core.service.impl;
 import java.util.List;
 
 import org.cactoos.list.ListOf;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import com.mitarcon.user.admin.core.model.Store;
@@ -20,6 +22,7 @@ public class StoreServiceImpl implements StoreService {
   }
 
   @Override
+  @Cacheable("store")
   public List<Store> findAll() {
     return new ListOf<Store>(storeRepository.findAll());
   }
@@ -30,11 +33,13 @@ public class StoreServiceImpl implements StoreService {
   }
 
   @Override
+  @CacheEvict(cacheNames = "store", allEntries = true)
   public Store create(Store store) {
     return storeRepository.save(store);
   }
 
   @Override
+  @CacheEvict(cacheNames = "store", allEntries = true)
   public Store update(Long id, Store store) {
     // TODO Auto-generated method stub
     return null;
